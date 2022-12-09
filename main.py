e804c5d77c69e773f1193f82b69d01a402974821
Ianc Radu <iancradu10@gmail.com>
2022-12-09 13:28:51 +0200

    update/ new function

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.relative_locator import locate_with
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys # used to press enter key
+from selenium.webdriver.common.keys import Keys  # used to press enter key
+from pyautogui import press, typewrite, hotkey
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+projects_assigned = {'R02426_Changchun VOLVO RE-DESIGN 1.2': [5.0, 0.0], 'R02395_AUD_MQB_21_DCU_DIS_CCQ': [3.0, 0.0],'R02245 AUD_XXXX_16_PNEU': [6.0, 0.0]}
+# projects_assigned = {'Internal: OEMs spec analysis': [5.0, 0.0]}
 
 
 class Booking:
@@ -24,7 +30,7 @@ class Booking:
         driver.maximize_window()
 
     def login(self):
-        # Get input field for user name and place name
+        # Get input field for username and place name
         driver.find_element(By.XPATH, '//*[@id="txtUserName"]').send_keys(self.name)
         driver.find_element(By.XPATH, '//*[@id="txtPassword"]').send_keys(self.password)
         driver.find_element(By.XPATH, '//*[@id="btnLogin"]').click()
@@ -57,11 +63,23 @@ class Booking:
         return all_projects
 
     def go_to_daily_bookings(self):
+
         driver.find_element(By.XPATH, '//*[@id="lbtnBooking"]').click()
         driver.implicitly_wait(3)
         driver.find_element(By.XPATH, '//*[@id="lbtnBooking"]').click()  # Button Bookings
         driver.implicitly_wait(3)
+
         #  ---------------------delete up to here after test-----------------
+        def add_hours(element, hours):
+            # Helper function
+            element.click()
+            time.sleep(1)
+            element.click()
+            time.sleep(2)
+            typewrite(f'{hours}')
+            time.sleep(1)
+            hotkey('Enter')
+
         driver.find_element(By.XPATH, '//*[@id="ext-comp-1018__tab0"]').click()  # Button DailyBookings
         time.sleep(15)
         driver.find_element(By.XPATH, '//*[@id="ext-gen37"]').click()  # Button Expand All
@@ -70,36 +88,49 @@ class Booking:
             if len(package.text) > 3:
                 if ', ' in package.text:
                     if len(work_packages[index + 3].text) > 3 and float(work_packages[
-                                                                            index + 4].text) > 0.01:  # Takes the value from the time difference column(index+3)
-                        # float(work_packages[index+3].text) -valoare pentru pontaj
+                                                                            index + 4].text) > 0.01:  # Takes the
+                        # value from the time difference column(index+3)
+                        # print(float(work_packages[index+3].text)) #-valoare pentru pontaj
                         extra = 0
-                        add_one_time = 1 # value added to offset first read by 1 index
-                        print(f'Start new day: {work_packages[index].text}')
-                        while ', ' not in work_packages[index + extra + add_one_time].text:
-                            print(f'{index+extra}:{work_packages[index + extra].text}')
-                            if work_packages[index + extra].text == 'R02426_Kaunas VOLVO RE-DESIGN 1.2':
-                                work_packages[index + extra+ 2].click()
-                                time.sleep(1)
-                                work_packages[index + extra + 2].click()
-                                time.sleep(2)
-                                work_packages[index + extra + 2].send_keys('1')
-                                time.sleep(1)
-                                # work_packages[index + extra + 2].send_keys(Keys.ENTER)
-                                print(work_packages[index + extra + 2].text)
-                                break
-
+                        add_one_time = 1  # value added to offset first read by 1 index
+                        print(
+                            f'Start new day: {work_packages[index].text} with {float(work_packages[index + 3].text)} hours')
+                        while ', ' and ' 20' not in work_packages[index + extra + add_one_time].text:
+                            # print(f'{index + extra}:{work_packages[index + extra].text}')
+                            # Checks the column with Date / Work packages and if it finds a project assigned to the
+                            # person and with enough hours left to book in work packages it will fill the entry form and
+                            # subtracts the time from the project assigned hours.
+                            name_of_project = work_packages[index + extra].text
+                            hours_worked = float(work_packages[index + 3].text)
+                            hours_left_to_book = hours_worked
+                            for project in projects_assigned:
+                                # print(f"{project} in list")
+                                if name_of_project in projects_assigned:
+                                    print(name_of_project)
+                                    project_assigned_hours_remaining = projects_assigned[f'{name_of_project}'][0]
+                                    if project_assigned_hours_remaining > 0.0:
+                                        print(
+                                            f'Found:{name_of_project} with {project_assigned_hours_remaining} hours remaining')
+                                        if hours_worked > project_assigned_hours_remaining:
+                                            add_hours(work_packages[index + extra + 2],
+                                                      project_assigned_hours_remaining)
+                                            projects_assigned[f'{name_of_project}'][1] += project_assigned_hours_remaining
+                                            projects_assigned[f'{name_of_project}'][0] = float(0)
+                                            hours_left_to_book = round((hours_worked - project_assigned_hours_remaining),2)
+                                        else:
+                                            add_hours(work_packages[index + extra + 2],
+                                                      hours_left_to_book)
+                                            projects_assigned[f'{name_of_project}'][1] = round((projects_assigned[f'{name_of_project}'][1]+hours_left_to_book),2)
+                                            projects_assigned[f'{name_of_project}'][0] = round((projects_assigned[f'{name_of_project}'][0]-hours_left_to_book),2)
+                                    break
                             add_one_time = 0
                             extra += 6
+        print(projects_assigned)
                 # print(f'Index is:{index} and value is{package.text}')
 
 
-# //*[@id="ext-gen37"] Button DailyBookings
-#  and "Test" not in package.text and "Assigned" not in package.text and "QL Testing" not in package.text
-# //*[@id="ext-comp-1018__tab0"] -daily bookings
-# //*[@id="lbtnUpdateOverview2"] -update overview
-
-nume = Booking('iancr')
-nume.login()
-# nume.go_to_project_workpackages()
-# nume.read_projects_workpackages()
-nume.go_to_daily_bookings()
+name = Booking('iancr')
+name.login()
+# name.go_to_project_workpackages()
+# name.read_projects_workpackages()
+name.go_to_daily_bookings()
